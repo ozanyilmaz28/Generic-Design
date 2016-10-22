b41947b62d8a3ecda7b8c50daf6907646383a05c
Ozan Yılmaz <ozany@la.com.tr>
2016-10-22 17:22:17 +0300

    AngularJs Added to Project

--- GenericDesign/Controllers/PlatformController.cs
+++ GenericDesign/Controllers/PlatformController.cs
@@ -8,7 +8,6 @@ namespace GenericDesign.Controllers
 {
     public class PlatformController : Controller
     {
-        // GET: Platform
         public ActionResult Index()
         {
             return View();
